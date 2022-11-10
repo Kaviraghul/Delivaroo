8cda8ba4a57d85a69f24698f53f71978de785626
Kaviraghul <vettrithamil@gmail.com>
2022-11-11 01:06:31 +0530

    added category section

---
+++ components/Categories.js
@@ -0,0 +1,25 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import CategoryCard from './CategoryCard'
+
+const Categories = () => {
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={
+        {
+            paddingHorizontal:15,
+            paddingTop:10
+        }
+    } >
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+        <CategoryCard imgUrl="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80" title="testing"/>
+
+    </ScrollView>
+  )
+}
+
+export default Categories
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, SafeAreaView, Image} from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView} from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {  ChevronDownIcon, SparklesIcon, UserIcon } from "react-native-heroicons/solid";
+import {  AdjustmentsIcon, ChevronDownIcon, SearchIcon, SparklesIcon, UserIcon } from "react-native-heroicons/solid";
+import Categories from '../components/Categories';
 
 export default function HomeScreen() {
   
@@ -21,12 +22,24 @@ export default function HomeScreen() {
                     />
                     <View className='flex-1'>
                         <Text className = ' font-bold text-gray-400 text-xs'>Deliver now!</Text>
-                        <Text className = 'font-bold flex-row align-middle text-xl'>Current Location 
-                        <ChevronDownIcon size={24} color= "#00CCBB"/>
-                    </Text>
+                        <Text className = 'font-bold text-xl flex-row items-center'>Current Location<ChevronDownIcon className='bg-red-400 pt-5' size={20}  color= "#00CCBB"/></Text>
                     </View>
                     <UserIcon size={35} color="#00CCBB"/>
                 </View>
+
+                {/* search */}
+
+                <View className='flex-row items-center space-x-2 pb-2 mx-4 '>
+                    <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+                     <SearchIcon color= "#00CCBB"/>
+                     <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+                    </View>
+                    <AdjustmentsIcon color= "#00CCBB" />
+                </View>
+
+                <ScrollView>
+                    <Categories />
+                </ScrollView>
     </SafeAreaView>
   )
 }
